872042e1676381a97321c3ea8b0906289c0531a7
jc-hiroto <taste.ful.msa@gmail.com>
2020-03-30 20:44:41 +0800

    Restore: GUI

--- src/home.java
+++ src/home.java
@@ -1,12 +1,12 @@
 package src;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.*;
 
 public class home {
-    private JPanel panel1;
+    private JPanel window;
     private JButton btnSearch;
     private JTextField searchField;
     private JButton btnRecommand;
@@ -14,10 +14,7 @@ public class home {
     private JButton btnList;
     private JButton btnExit;
     private JButton btnSettings;
-    private JButton btnInfo;
     private JButton btnLogin;
-<<<<<<< HEAD
-<<<<<<< HEAD
     private JPanel Home;
     private JPanel bottom;
     private JPanel homeContentHolder;
@@ -26,26 +23,14 @@ public class home {
     private JPanel cardHolder;
     private JPanel aboutPanel;
     private JButton btnHome;
-    private JButton btnAbout;
-    private JPanel registerPanel;
-=======
->>>>>>> parent of f6adda2... 20200330: GUI update.
-=======
->>>>>>> parent of f6adda2... 20200330: GUI update.
     public static String metalUI = "javax.swing.plaf.metal.MetalLookAndFeel";
-    public CardLayout layout = null;
 
     public home() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        cardInit();
+        cardHolder.add(homePanel, "Home");
+        cardHolder.add(loginPanel, "Login");
+        cardHolder.add(aboutPanel, "About");
+        CardLayout layout = (CardLayout)cardHolder.getLayout();
         btnHome.setVisible(false);
-=======
-
->>>>>>> parent of f6adda2... 20200330: GUI update.
-=======
-
->>>>>>> parent of f6adda2... 20200330: GUI update.
         btnExit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
@@ -56,7 +41,7 @@ public class home {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 String searchContent = getSearchField();
-                System.out.println("Search: "+searchContent);
+                System.out.println("Search: " + searchContent);
             }
         });
         btnRecommand.addActionListener(new ActionListener() {
@@ -77,65 +62,25 @@ public class home {
 
             }
         });
-<<<<<<< HEAD
-<<<<<<< HEAD
         btnLogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent){
                 layout.show(cardHolder, "Login");
-                exitFromHome();
+                btnLogin.setVisible(false);
+                btnHome.setVisible(true);
             }
         });
         btnHome.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                backToHome();
+                layout.show(cardHolder, "Home");
+                btnHome.setVisible(false);
                 btnLogin.setVisible(true);
             }
         });
-        btnAbout.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                layout.show(cardHolder,"About");
-                exitFromHome();
-            }
-        });
-    }
-    public void goToRegister(){
-        layout.show(cardHolder,"Register");
-        exitFromHome();
     }
 
-    public void cardInit(){
-        cardHolder.add(homePanel, "Home");
-        cardHolder.add(loginPanel, "Login");
-        cardHolder.add(registerPanel, "Register");
-        cardHolder.add(aboutPanel, "About");
-
-        layout = (CardLayout)cardHolder.getLayout();
-    }
-
-    public void backToHome(){
-        layout.show(cardHolder, "Home");
-        btnHome.setVisible(false);
-        btnAbout.setVisible(true);
-        btnSettings.setVisible(true);
-    }
-
-    public void exitFromHome(){
-        btnHome.setVisible(true);
-        btnLogin.setVisible(false);
-        btnSettings.setVisible(false);
-        btnAbout.setVisible(false);
-=======
->>>>>>> parent of f6adda2... 20200330: GUI update.
-    }
-
-=======
-    }
-
->>>>>>> parent of f6adda2... 20200330: GUI update.
-    public String getSearchField(){
+    public String getSearchField() {
         return searchField.getText();
     }
 
@@ -146,18 +91,14 @@ public class home {
             e.printStackTrace();
         }
         JFrame frame = new JFrame("Home");
-<<<<<<< HEAD
-<<<<<<< HEAD
-        frame.setSize(1000,800);
         frame.setContentPane(new home().window);
-=======
-        frame.setContentPane(new home().panel1);
->>>>>>> parent of f6adda2... 20200330: GUI update.
-=======
-        frame.setContentPane(new home().panel1);
->>>>>>> parent of f6adda2... 20200330: GUI update.
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
         frame.setVisible(true);
     }
 
+    private void createUIComponents() {
+        loginPanel = new login().getPanel();
+        aboutPanel = new about().getPanel();
+    }
 }
--- src/login.java
+++ src/login.java
@@ -1,40 +1,15 @@
 package src;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class login {
     private JPanel contentHolder;
     private JLabel loginLabel;
     private JTextField textField1;
     private JPasswordField passwordField1;
-    private JButton btnLogin;
-    private JButton btnForgot;
-    private JButton btnRegister;
-    private JPanel loginHolder;
-    private JPanel loginPanel;
-    private JPanel registerPanel;
-
-    public login() {
-        loginHolder.add(loginPanel, "Login");
-        loginHolder.add(registerPanel, "Register");
-        CardLayout loginLayout = (CardLayout)loginHolder.getLayout();
-        btnRegister.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                loginLayout.show(loginHolder,"Register");
-            }
-        });
-
-    }
+    private JButton button1;
 
     public JPanel getPanel(){
         return contentHolder;
-    }
-
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
     }
 }
