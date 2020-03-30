0b89383b808aa2e04afdf75428db5053a54985b9
jc-hiroto <taste.ful.msa@gmail.com>
2020-03-30 22:57:17 +0800

    20200330-3: GUI update.
    - 增加註冊頁面

--- src/login.java
+++ src/login.java
@@ -1,6 +1,7 @@
 package src;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,20 +11,38 @@ public class login {
     private JTextField textField1;
     private JPasswordField passwordField1;
     private JButton btnLogin;
-    private JButton btnForgot;
     private JButton btnRegister;
-    private JButton button1;
+    private JPanel loginCardHolder;
+    private JPanel loginPanel;
+    private JPanel registerPanel;
+    private JButton btnBack;
 
     public login() {
+        loginCardHolder.add(loginPanel, "Login");
+        loginCardHolder.add(registerPanel, "Register");
+        CardLayout layout = (CardLayout)loginCardHolder.getLayout();
         btnRegister.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                layout.show(loginCardHolder, "Register");
+                btnBack.setVisible(true);
 
+            }
+        });
+        btnBack.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                layout.show(loginCardHolder, "Login");
+                btnBack.setVisible(false);
             }
         });
     }
 
     public JPanel getPanel(){
         return contentHolder;
+    }
+
+    private void createUIComponents() {
+        registerPanel = new register().getPanel();
     }
 }
