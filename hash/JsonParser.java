a268c7fb33b89d45a71da6dac868d02eb940a47c
Ray Kao <raykao77@gmail.com>
2020-04-04 14:12:36 +0800

    password hash/ Json parser update

---
+++ hash/JsonParser.java
@@ -0,0 +1,41 @@
+import java.io.FileNotFoundException;
+import java.lang.StringIndexOutOfBoundsException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.JSONException;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+public class ReadJsonFile{
+	
+	@SuppressWarnings("unchecked");
+	JSONParser jsonParser = new JSONParser();
+
+
+	try(FileReader reader = new FileReader("travel_code.json")){
+		JSONOArray rawTravelCodeList = jsonParser.parser(reader);
+		rawTravelCodeList.forEach(tvlc -> ParseTravelCodeObject((JSONObject) tvlc));
+	}catch(FileNotFoundException){
+		e.printStackTrace();
+	}catch(IOException){
+		e.printStackTrace();
+	}catch(ParseException){
+		e.printStackTrace();
+	}
+
+}
+
+private static class ParseTravelCodeObject(JSONObject travelCode){
+	
+}
+
+public class JsonParser{
+	
+	public String[] getTravelCodeInfo(JSONObject JsonParser.ReadJsonFile){
+
+	}
+}
---
+++ hash/passwordHash.java
@@ -0,0 +1,64 @@
+//AES ECB PKCS7 padding mode
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import static org.apache.commons.codec.binary.Base64.decodeBase64;
+import static org.apache.commons.codec.binary.Base64.encodeBase64;
+
+public class passwordHash {
+	
+    private final static String alg = "AES";
+    private final static String cI = "AES/ECB/PKCS7Padding";
+    private final static String key = "0123456789ABCDEF";
+
+    //encode text function
+    public StringBuffer encrypt(StringBuffer plainText) throws Exception {
+	    
+		try {
+    		//generate the size of input text and key
+    		byte[] input = String.valueOf(plainText).getBytes();
+	        byte[] keyBytes = key.getBytes();
+	        
+    		Cipher cipher = Cipher.getInstance(cI, "BC");
+            SecretKeySpec skeySpec = new SecretKeySpec(keyBytes, alg);
+            
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+            byte[] encrypted = new byte[cipher.getOutputSize(input.length)];
+            int ctLength = cipher.update(input, 0, input.length, encrypted, 0);
+            ctLength += cipher.doFinal(encrypted, ctLength);
+            
+            String s = new String(encodeBase64(encrypted));
+            return new StringBuffer(s);
+		}catch(Exception e) {
+			e.getStackTrace();
+			throw e;
+		}
+    		
+    }
+    
+    //decode text function
+    public StringBuffer decrypt(StringBuffer cipherText) throws Exception {
+            
+    	try {
+    		//generate the size of input text and key
+    		byte[] input = decodeBase64(String.valueOf(cipherText));
+	        byte[] keyBytes = key.getBytes();
+	        
+    		Cipher cipher = Cipher.getInstance(cI, "BC");
+            SecretKeySpec skeySpec = new SecretKeySpec(keyBytes, alg);
+            
+            int ctLength = input.length;
+            
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+        	byte[] decrypted = new byte[cipher.getOutputSize(ctLength)];
+            int ptLength = cipher.update(input, 0, ctLength, decrypted, 0);
+            ptLength += cipher.doFinal(decrypted, ptLength);
+            
+            String s = new String(decrypted, "UTF-8");
+            return new StringBuffer(s);
+    	}catch(Exception e) {
+    		e.getStackTrace();
+    		throw e;
+		}
+    }
+    
+}
